88f8a6cc5771499a23c94b4f092728d790a60f06
Dana <allmightallcan@gmail.com>
2022-02-03 01:08:23 +0600

    Добавила комментарии и подправила код стайл в Уроке 4

--- Урок 4 - Стартуем в Codeception/tests/acceptance/CheckBlouseCest.php
+++ Урок 4 - Стартуем в Codeception/tests/acceptance/CheckBlouseCest.php
@@ -1,26 +1,20 @@
 <?php
-
+/**
+ * Класс для проверки быстрого просмотра
+ */
 class CheckBlouseCest
 {
+    /**
+     * Проверка быстрого просмотра у товара "Blouse"
+     */
     public function checkBlouse(AcceptanceTester $I)
     {
-        # Зайти на http://automationpractice.com/index.php
         $I->amOnPage('');
-
-        # Найти на странице товар «Blouse»
         $I->scrollTo('#homefeatured > li:nth-child(2)');
-
-        # Навести на него мышью
         $I->moveMouseOver('#homefeatured > li:nth-child(2)');
-
-        #Кликнуть на кнопку «Quick view» внутри блока с товаром
         $I->click('#homefeatured > li:nth-child(2) > div > div.left-block > div > a.quick-view');
-
-        # Дождаться появления модального окна
         $I->waitForElementVisible('.fancybox-iframe');
         $I->switchToIFrame('.fancybox-iframe');
-
-        # Убедиться, что это нужный нам товар, проверив текст «Blouse» внутри модального окна
         $I->waitForText('Blouse',20,'#product > div > div > div.pb-center-column.col-xs-12.col-sm-4 > h1');
     }
 }
--- Урок 4 - Стартуем в Codeception/tests/functional/SearchPrintedDressCest.php
+++ Урок 4 - Стартуем в Codeception/tests/functional/SearchPrintedDressCest.php
@@ -1,20 +1,18 @@
 <?php
-
+/**
+ * Класс для проверки поиска
+ */
 class SearchPrintedDressCest
 {
+    /**
+     * Проверка поиска товара «Printed dress»
+     */
     public function searchPrintedDress(AcceptanceTester $I)
     {
-        # 1. Зайти на http://automationpractice.com/index.php
         $I->amOnPage('');
-
-        # 2. В поле поиска написать «Printed dress»
         $I->click('#search_query_top');
         $I->fillField("//input[@id='search_query_top']", 'Printed Dress');
-
-        # 3. Кликнуть на кнопку поиска
         $I->click('#searchbox > button');
-
-        # 4. На странице результатов убедиться, что было найдено 5 товаров (считать карточки товаров)
         $I->seeNumberOfElements('#center_column > .product_list > .ajax_block_product > .product-container', '5');
     }
 }
